327d6cc0f2a7b3abbcc8cccad2492cbdcb19c547
JulioMike <julio.oliveira@ee.ufcg.edu.br>
2021-04-25 20:36:02 -0300

    subindo a forma de baixar o model_v3.h5

---
+++ gdrive.py
@@ -0,0 +1,4 @@
+from google_drive_downloader import GoogleDriveDownloader as gdd
+gdd.download_file_from_google_drive(file_id='17HKIsYWzhQ3Wn5IHsc_Iu3K8EngnYp3d',
+                                    dest_path='.\model_v3.h5',
+                                    unzip=False)
