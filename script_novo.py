c041ad9f519bb784aa2bbcf1a358d6e7c08b9ce0
JulioMike <julio.oliveira@ee.ufcg.edu.br>
2021-06-30 11:01:21 -0300

    ajeitei todo o python

--- script_novo.py
+++ script_novo.py
@@ -125,8 +125,21 @@ collection.insert_one( query_nova );
 
 # Limpeza de memoria
 
-comando = "sudo ./limpezaCacheSwap.sh"
-output = subprocess.check_output(['bash','-c',comando])
+comando = "echo ---- Limpeza Cache e Swap ----"
+subprocess.Popen(comando,shell=True)
+comando2 = "free -h"
+subprocess.Popen(comando2, shell=True)
+comando3 = "echo 3 > /proc/sys/vm/drop_caches"
+subprocess.Popen(comando3, shell=True)
+comando4 = "sysctl -w vm.drop_caches=3 "
+subprocess.Popen(comando4, shell=True)
+comando5 = "swapoff -a && swapon -a"
+subprocess.Popen(comando5, shell=True)
+comando6 = "clear"
+subprocess.Popen(comando6, shell=True)
+comando7 = "echo Limpeza do Cache e Swap efetuada com sucesso"
+subprocess.Popen(comando7, shell=True)
+comando8 = "free -h"
+subprocess.Popen(comando8, shell=True)
 
-memoria = "free -h"
-output = subprocess.check_output(['bash','-c',memoria])
+
--- teste.py
+++ teste.py
@@ -1,10 +1,19 @@
 import subprocess
-import sys
 
-comando = "echo Limpeza Cache e Swap"
+comando = "echo ---- Limpeza Cache e Swap ----"
 subprocess.Popen(comando,shell=True)
 comando2 = "free -h"
 subprocess.Popen(comando2, shell=True)
 comando3 = "echo 3 > /proc/sys/vm/drop_caches"
 subprocess.Popen(comando3, shell=True)
+comando4 = "sysctl -w vm.drop_caches=3 "
+subprocess.Popen(comando4, shell=True)
+comando5 = "swapoff -a && swapon -a"
+subprocess.Popen(comando5, shell=True)
+comando6 = "clear"
+subprocess.Popen(comando6, shell=True)
+comando7 = "echo Limpeza do Cache e Swap efetuada com sucesso"
+subprocess.Popen(comando7, shell=True)
+comando8 = "free -h"
+subprocess.Popen(comando8, shell=True)
 
