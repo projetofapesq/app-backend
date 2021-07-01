5b3d82c834ed11f7bb886c220d05c9aa55fc22b5
root <root@debian.cloud.server>
2021-06-30 21:18:56 -0300

    resolvendo problemas

--- script_novo.py
+++ script_novo.py
@@ -127,18 +127,19 @@ collection.insert_one( query_nova );
 
 comando = "echo ---- Limpeza Cache e Swap ----"
 subprocess.Popen(comando,shell=True)
-comando2 = "free -h"
-subprocess.Popen(comando2, shell=True)
+#comando2 = "free -h"
+#subprocess.Popen(comando2, shell=True)
 comando3 = "echo 3 > /proc/sys/vm/drop_caches"
 subprocess.Popen(comando3, shell=True)
 comando4 = "sysctl -w vm.drop_caches=3 "
 subprocess.Popen(comando4, shell=True)
 comando5 = "swapoff -a  && sudo swapon /swapfile"
 subprocess.Popen(comando5, shell=True)
-comando6 = "clear"
-subprocess.Popen(comando6, shell=True)
+#comando6 = "clear"
+#subprocess.Popen(comando6, shell=True)
 comando7 = "echo Limpeza do Cache e Swap efetuada com sucesso"
 subprocess.Popen(comando7, shell=True)
-
+#comando8 = "free -h"
+#subprocess.Popen(comando8, shell=True)
 
 
