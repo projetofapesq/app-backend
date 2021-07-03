7e3d818c280c348dc7b2fb049b7fe55821d25427
root <root@debian.cloud.server>
2021-07-03 14:43:14 -0300

    subindo as alterações

--- teste.js
+++ teste.js
@@ -1,6 +1,15 @@
 const {PythonShell} = require('python-shell');//para ligar o python ao nodejs
 
-PythonShell.run('script_clean.py',null,(err)=>{
-    if (err) throw err;
-    console.log('#######  LIMPEZADO DO BD! TERMINADO!  #######')
+
+let pyshell = new PythonShell('script_clean.py');
+
+pyshell.send('hello');
+
+pyshell.on('message', (message)=>{
+	console.log(message);
+});
+
+pyshell.end((err)=>{
+	if(err)throw err;
+	console.log('finished');
 })
