1cbfd032f9a555400b60170b6bc79ee8026f3e22
unknown <projetofapesq>
2021-07-15 10:36:51 -0300

    ajeitei o gdrive

--- script_gdrive.py
+++ script_gdrive.py
@@ -1,4 +1,4 @@
 from google_drive_downloader import GoogleDriveDownloader as gdd
 gdd.download_file_from_google_drive(file_id='1FIOIAL8X0oNqhTiq05CuC0LGtoqu2ihN',
-                                    dest_path='.\diagnosis-final\app-backend\model_v7.h5',
+                                    dest_path='./model_v7.h5',
                                     unzip=False)
